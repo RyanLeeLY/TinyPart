f96f3cd028adc1788783c556724de2c308dd8a68
yao.li <yao.li@cootek.cn>
2019-09-12 11:01:00 +0800

    [chore] add tests

---
+++ TinyPartTests/TinyPartTests.m
@@ -0,0 +1,37 @@
+//
+//  TinyPartTests.m
+//  TinyPartTests
+//
+//  Created by Yao Li on 2019/9/12.
+//  Copyright © 2019 yaoli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TinyPartTests : XCTestCase
+
+@end
+
+@implementation TinyPartTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
